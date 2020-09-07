b674bf2ad7e818ba703995971714fea45d5ceb6a
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-09-07 10:06:23 -0700

    React initial setup with babel to transpile

---
+++ compiled/src/components/App.js
@@ -0,0 +1,16 @@
+// import React from 'react';
+
+// function App() {
+//   // console.log('App console')
+//   return (
+//     <h1>Hello, world!</h1>
+//   )
+// }
+var App = props => React.createElement(
+  "h1",
+  null,
+  "Hello App"
+);
+
+export default App;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uLy4uL3NyYy9jb21wb25lbnRzL0FwcC5qc3giXSwibmFtZXMiOlsiQXBwIiwicHJvcHMiXSwibWFwcGluZ3MiOiJBQUFBOztBQUVBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBLElBQUlBLE1BQU9DLEtBQUQsSUFDUjtBQUFBO0FBQUE7QUFBQTtBQUFBLENBREY7O0FBSUEsZUFBZUQsR0FBZiIsImZpbGUiOiJBcHAuanMiLCJzb3VyY2VzQ29udGVudCI6WyIvLyBpbXBvcnQgUmVhY3QgZnJvbSAncmVhY3QnO1xuXG4vLyBmdW5jdGlvbiBBcHAoKSB7XG4vLyAgIC8vIGNvbnNvbGUubG9nKCdBcHAgY29uc29sZScpXG4vLyAgIHJldHVybiAoXG4vLyAgICAgPGgxPkhlbGxvLCB3b3JsZCE8L2gxPlxuLy8gICApXG4vLyB9XG52YXIgQXBwID0gKHByb3BzKSA9PiAoXG4gIDxoMT5IZWxsbyBBcHA8L2gxPlxuKVxuXG5leHBvcnQgZGVmYXVsdCBBcHA7XG5cbiJdfQ==
---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+// import React from 'react';
+
+// function App() {
+//   // console.log('App console')
+//   return (
+//     <h1>Hello, world!</h1>
+//   )
+// }
+var App = (props) => (
+  <h1>Hello App</h1>
+)
+
+export default App;
+
