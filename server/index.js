163bbda351b3978b132c1d1f581e0ee9d50d024f
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-09-15 18:54:54 -0700

    without controller and model

--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ const path = require('path');
 const app = express();
 //*
 // const db = require('../database')   // by requiring db in server, help db to server? No! model requiring database there to connect
-const Controller = require('./Controller.js') // interpreter going Controller
+const db = require('../database')
+// const Controller = require('./Controller.js') // interpreter going Controller
 const port = 3000
 const bodyParser = require('body-parser')
 
@@ -21,28 +22,43 @@ app.use('/', express.static(path.join(__dirname, '../client/dist')))
 // })
 
 // With model/conntroller
-app.get('api/movies', Controller.getAllMovies);
-app.post('/api/movies', Controller.postNewMovie);
+// app.get('api/movies', Controller.getAllMovies);
+// app.post('/api/movies', Controller.postNewMovie);
 // app.get('api/movies/', Controller.searchMovies);
 
 
-// Without model/conntroller
-/*
+// ***** Without model/conntroller
+// GET ALL MOVIES
 app.get('/api/movies', (req, res) => {
-  console.log('req.body', req.body)
-  const { title, watched } = req.body
-app
-  const q = `INSERT INTO groceries (title, wtched) VALUES (?, ?)`;
+  let q = 'SELECT * FROM movies'
 
-  db.connection.query(q, [title, watched], (err, data) => {
+  db.connection.query(q, (err, data) => {
     if (err) {
+      console.log('CONTROLLER GET MOVIES SUCCESS- response')
       res.status(400).send();
     } else {
-      res.status(201).send()
+      console.log('CONTROLLER GET MOVIES SUCCESS- response', data)
+      res.status(200).send(data);
     }
   })
 })
-*/
+
+// POST NEW MOVIE
+app.post('/api/movies', (req, res) => {
+  console.log('req.body', req.body)
+  const { title, watched } = req.body
+  const q = `INSERT INTO movies (title, watched) VALUES (?, ?)`;
+
+  db.connection.query(q, [title, watched], (err, data) => {
+    if (err) {
+      console.log('CONTROLLER POST MOVIES- ERR', err)
+      res.status(400).send();
+    } else {
+      console.log('CONTROLLER POST MOVIES SUCCESS- response')
+      res.status(200).send('post success');
+    }
+  })
+})
 
 
 app.listen(port, () => {
